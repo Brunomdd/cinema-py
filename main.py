16c1fc250f5dfb99eaa0857240a28836f286bfdd
Brunomdd <brunnodiniiz@gmail.com>
2026-07-31 11:47:45 -0300

    feat and refactor: adiciona opção de mostrar a estatisticas e corrije erro de indíce na função obter indíce

--- main.py
+++ main.py
@@ -4,8 +4,17 @@
 
 def leia_int(num):
     return executar(int,num)
+
+def linha(tam=32):
+    return '-'*tam
+
+def cabecalho(txt):
+    print(linha())
+    print(f'{txt}'.center(32))
+    print(linha())
+
+
 def criar_sala():
-    
     sala = []
     for fileiras in range(8):
         fileira = []
@@ -15,6 +24,7 @@ def criar_sala():
     return sala
 
 def mostrar_sala(sala):
+    cabecalho('MAPA DO SISTEMA DE CINEMA')
     letras = ['A','B','C','D','E','F','G','H']
     sala_visual = []
     for fileira in sala:
@@ -42,6 +52,7 @@ def executar(funcao,num):
 
 
 
+
 def criar_indice():
     nome = leia_string('Fileira: ')
     if not nome:
@@ -51,31 +62,59 @@ def criar_indice():
         print('ERRO: o indíce é invalido.')
         return
     indice_fileira = ord(nome) - ord('A')
+    if indice_fileira >7:
+        print('ERRO: número totalmente fora de intervalo!')
+        return
+    
+
     indice_assento = numero -1
     return indice_fileira,indice_assento
 
 def fazer_reserva(sala):
+    cabecalho('FAZER RESERVA')
     resultado = criar_indice()
     if resultado:
         indice_fileira,indice_assento = resultado
-    if sala[indice_fileira][indice_assento] == 0:
-        sala[indice_fileira][indice_assento] =1
-        print('Reservado com sucesso!')
-    else:
-        print('Não é possivel fazer a reserva')
+        if sala[indice_fileira][indice_assento] == 0:
+            sala[indice_fileira][indice_assento] =1
+            print('Reservado com sucesso!')
+        else:
+            print('Não é possivel fazer a reserva')
+            return
+    return
 
 def cancelar_reserva(sala):
+    cabecalho('CANCELAR RESERVA')
     resultado = criar_indice()
     if resultado:
         indice_fileira,indice_assento = resultado
     if sala[indice_fileira][indice_assento] == 1:
             sala[indice_fileira][indice_assento] =0
             print('Reserva cancelada com sucesso!')
-
     else:
         print('não é possivel cancelar essa reserva.')
+def mostrar_estatisticas(sala):
+    dados = {'total':0,
+             'livres':0,
+             'ocupados':0,}
     
-  
+    for fileira in sala:
+        for assento in fileira:
+            dados['total'] +=1
+            if assento == 0:
+                dados['livres'] +=1
+            elif assento == 1:
+                dados['ocupados'] +=1
+    if dados['total'] >0:
+        percentual = (dados['ocupados']/dados['total'])*100
+        print(f"Percentual de ocupação: {percentual:.2f}%")
+        print(f"Total: {dados['total']}")
+        print(f"Lugares livres {dados['livres']}")
+        print(f"lugares ocupados: {dados['ocupados']}")
+
+
+
+    
 def leia_int(num):
     return executar(int,num)
 def leia_string(msg):
@@ -83,15 +122,19 @@ def leia_string(msg):
 
 
 def main():
+    cabecalho('cinema py')
     sala = criar_sala()
     while True:
-        opc = leia_int('Digite um número: ')
+        opc = leia_int('digite uma opção: ')
         if opc ==1:
             fazer_reserva(sala)
         elif opc == 2:
             cancelar_reserva(sala)
         elif opc == 3:
             mostrar_sala(sala)
+        elif opc == 4:
+            mostrar_estatisticas(sala)
+
              
         
 main()
