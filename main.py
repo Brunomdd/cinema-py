23c89dbeb3332030a15c4edfdb03a3785a6352e6
Brunomdd <brunnodiniiz@gmail.com>
2026-07-31 10:42:35 -0300

    feat: adiciona opções para o usuário fazer uma reserva ou cancelar uma reserva.

--- main.py
+++ main.py
@@ -40,8 +40,12 @@ def executar(funcao,num):
     except ValueError:
         print('o valor que o usuário digitou é inválido.')
 
-def criar_indice(sala):
+
+
+def criar_indice():
     nome = leia_string('Fileira: ')
+    if not nome:
+        return
     numero = leia_int('Assento: ')
     if numero <=0 or numero > 10:
         print('ERRO: o indíce é invalido.')
@@ -50,24 +54,46 @@ def criar_indice(sala):
     indice_assento = numero -1
     return indice_fileira,indice_assento
 
+def fazer_reserva(sala):
+    resultado = criar_indice()
+    if resultado:
+        indice_fileira,indice_assento = resultado
+    if sala[indice_fileira][indice_assento] == 0:
+        sala[indice_fileira][indice_assento] =1
+        print('Reservado com sucesso!')
+    else:
+        print('Não é possivel fazer a reserva')
 
+def cancelar_reserva(sala):
+    resultado = criar_indice()
+    if resultado:
+        indice_fileira,indice_assento = resultado
+    if sala[indice_fileira][indice_assento] == 1:
+            sala[indice_fileira][indice_assento] =0
+            print('Reserva cancelada com sucesso!')
+
+    else:
+        print('não é possivel cancelar essa reserva.')
+    
+  
 def leia_int(num):
     return executar(int,num)
 def leia_string(msg):
     return executar(str,msg)
+
 
 def main():
     sala = criar_sala()
     while True:
         opc = leia_int('Digite um número: ')
         if opc ==1:
-            mostrar_sala(sala)
+            fazer_reserva(sala)
         elif opc == 2:
-             sala = criar_indice(sala)
-             print(sala)
-            
-
-
+            cancelar_reserva(sala)
+        elif opc == 3:
+            mostrar_sala(sala)
+             
+        
 main()
 
 
