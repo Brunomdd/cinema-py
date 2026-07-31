9ba0041d78ff8b697872e27785d208762e209d7d
Brunomdd <brunnodiniiz@gmail.com>
2026-07-30 22:56:15 -0300

    feat: adiciona função que exibe a sala de cinema para o usuário

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+
 def criar_sala():
     sala = []
-    for c in range(8):
-        lista = []
-        for i in range(10):
-            lista.append(0)
-        sala.append(lista)
+    for fileiras in range(8):
+        fileira = []
+        for assento in range(10):
+            fileira.append(0)
+        sala.append(fileira)
     return sala
 
-sala = criar_sala()
-print(sala)
+def mostrar_sala(sala):
+    letras = ['A','B','C','D','E','F','G','H']
+    sala_visual = []
+    for fileira in sala:
+        fileira_visual = []
+        for assento in fileira:
+            if assento == 0:
+                fileira_visual.append('O')
+            elif assento == 1:
+                fileira_visual.append('X')
+
+        sala_visual.append(fileira_visual)
+    for pos,valor in enumerate(sala_visual):
+        print(letras[pos],' '.join(valor))
+        
+
+
+def main():
+    sala = criar_sala()
+    while True:
+        opc = int(input('Escolha uma opção: '))
+        if opc ==1:
+            mostrar_sala(sala)
+
+main()
+
+
+
+
 
 
